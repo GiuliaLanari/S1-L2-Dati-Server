4cf06a9dd5bf8b7c134cfc7a2e666c4fcb033ca0
GiuliaLanari <giulia.lanari96@gmail.com>
2024-04-10 16:44:02 +0200

    Add Style

--- success-registrazione.php
+++ success-registrazione.php
@@ -6,8 +6,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Registrazione completata!</title>
 </head>
-<body class="bg-black text-white">
-<h1 class="d-flex justify-content-center my-4">La tua registrazione è andata a buon fine!!</h1>
+<body class=" bg-black text-white">
+<h1 class="d-flex justify-content-center my-4  ">La tua registrazione è andata a buon fine!!</h1>
+ <div class="d-flex justify-content-center align-items-center">
+ <span style='font-size:100px;' >&#129321;</span>
+<span style='font-size:100px;'>&#127881;</span>
+
+ </div>
 
 
 
